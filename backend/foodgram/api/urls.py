32355f33bba69df4b6ed367369d21b6e8dc99259
Дмитрий Капустин <kda99@mail.ru>
2023-09-04 02:31:36 +0300

    1. Поднял фронт и nginx в Docker 2. Создал Django проект, приложения и superuser 3. Организовал связь docker-nginx - django 4. Тестовая view-функция c выводом текста на назначенный endpoint /index

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from django.urls import path, include
+
+from api.views import index
+router = routers.DefaultRouter()
+urlpatterns = [
+     path('index', index)
+ ]
