fa275ff4f52a0d9b9ccfa7f5fe9c92c8c14c81c3
Дмитрий Капустин <kda99@mail.ru>
2023-10-07 20:06:13 +0300

    поправил модели User и Subscripsion

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,30 +1,9 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db.models import UniqueConstraint
 from django.db import models
 
 
-# class UserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         if password:
-#             user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, password, **extra_fields)
-
-
 class User(AbstractUser):
-
-
     email = models.EmailField(
         'Электронная почта',
         max_length=254,
@@ -53,19 +32,12 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
-    # def save(self, *args, **kwargs):
-    #     if not self.pk:  # Если это новый пользователь
-    #         self.set_password(self.password)
-    #     super().save(*args, **kwargs)
-
-
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
 
 
 class Subscription(models.Model):
-
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -78,7 +50,6 @@ class Subscription(models.Model):
         related_name='followed',
         verbose_name='Подписываемый автор',
     )
-
     class Meta:
         constraints = [
             UniqueConstraint(fields=['user', 'author'],
