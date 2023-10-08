513c5468aee956923253a5a013c81e309ba71785
Дмитрий Капустин <kda99@mail.ru>
2023-10-08 22:42:44 +0300

    Ревью 0 recipes/models.py         media         api/permissions.py

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -1,60 +1,9 @@
-# from rest_framework import permissions
-#
-# from users.models import User
+from rest_framework import permissions
 
 
-# class IsAuthorOrReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return (request.method in permissions.SAFE_METHODS
-#                 or request.user.is_authenticated)
-#
-#     def has_object_permission(self, request, view, obj):
-#         if self.action == 'retrieve' and isinstance(obj, User):
-#             return True
-#         return (
-#             request.method in permissions.SAFE_METHODS
-#             or obj.author == request.user
-#         )
+class IsAuthorOrSU(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated or request.user.is_staff)
 
-
-# class UserPermission(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         if view.action == 'create':
-#             return True
-#         elif view.action == 'list':
-#             # Логика для разрешения действия "list"
-#             return True
-#         elif view.action == 'retrieve':
-#             # Логика для разрешения действия "retrieve"
-#             return True
-#         elif view.action == 'update':
-#             # Логика для разрешения действия "update"
-#             return True
-#         elif view.action == 'partial_update':ons
-#             # Логика для разрешения действия "partial_update"
-#             return True
-#         elif view.action == 'destroy':
-#
-#             return True
-#
-#     def has_object_permission(self, request, view, obj):
-#         if view.action == 'retrieve':
-#             # Логика для разрешения действия "retrieve"
-#             return True
-#         elif view.action == 'update':
-#             # Логика для разрешения действия "update"
-#             return True
-#         elif view.action == 'partial_update':
-#             # Логика для разрешения действия "partial_update"
-#             return True
-#         elif view.action == 'destroy':
-#             # Логика для разрешения действия "destroy"
-#             return True
-#
-#
-# class IsAuthorOrSU(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return (request.user.is_authenticated or request.user.is_staff)
-#
-#     def has_object_permission(self, request, view, obj):
-#         return obj.author == request.user or request.user.is_staff
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.user.is_staff
