11f9165e4acceabf356b1c69c38920b723dc1c10
Дмитрий Капустин <kda99@mail.ru>
2023-10-08 14:50:02 +0300

    Ревью 0

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',]
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', ]
 
 
 STATIC_URL = '/static/'
