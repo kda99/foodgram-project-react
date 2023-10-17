454e66134044fd4d44256b6e07c0bc27fc8143f3
Дмитрий Капустин <kda99@mail.ru>
2023-10-17 23:19:28 +0300

    Deploy 0

--- backend/foodgram/api/scripts/load_data.py
+++ backend/foodgram/api/scripts/load_data.py
@@ -1,8 +1,7 @@
 import json
 import os
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
 
 from recipes.models import Ingredient, Tag
@@ -13,10 +12,9 @@ class Command(BaseCommand):
 
     def run():
         ingredients_file = 'ingredients.json'
-        # tags_file = 'tags.json'
 
         try:
-            with open(os.path.join(settings.MEDIA_ROOT, 'data', ingredients_file), 'r', encoding='utf-8') as f:
+            with open(ingredients_file) as f:
                 data = json.load(f)
                 for ingredient in data:
                     try:
@@ -24,14 +22,6 @@ class Command(BaseCommand):
                                                   measurement_unit=ingredient['measurement_unit'])
                     except IntegrityError:
                         print(f'В базе уже есть: {ingredient["name"]} ({ingredient["measurement_unit"]})')
-
-            # with open(os.path.join(settings.MEDIA_ROOT, 'data', tags_file), 'r', encoding='utf-8') as f:
-            #     data_tags = json.load(f)
-            #     for tag in data_tags:
-            #         try:
-            #             Tag.objects.create(name=tag['name'], color=tag['color'], slug=tag['slug'])
-            #         except IntegrityError:
-            #             print(f'В базе уже есть: {tag["name"]}')
 
         except FileNotFoundError:
             print('Файл отсутствует в директории media/data')
