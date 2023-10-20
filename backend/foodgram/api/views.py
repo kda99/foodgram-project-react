8746aa622a57c60e292e2a9c7d735759b1ef7e71
Дмитрий Капустин <kda99@mail.ru>
2023-10-20 12:21:41 +0300

    Deploy 0 ингредиенты

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -6,7 +6,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
 from rest_framework import permissions, status
 from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
