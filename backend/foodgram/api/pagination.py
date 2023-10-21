7fe4acade891ddf33e4eb50ce95d9c7c3a4075be
Дмитрий Капустин <kda99@mail.ru>
2023-10-21 20:20:20 +0300

    Deploy 1

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     page_size = 6
+    custom_limit = 999
 
     def paginate_queryset(self, queryset, request, view=None):
         if ('limit' in request.query_params and 'is_in_shopping_cart'
@@ -11,5 +12,5 @@ class CustomPagination(PageNumberPagination):
             is_in_shopping_cart = request.query_params.get(
                 'is_in_shopping_cart')
             if limit in ('999', '6') and is_in_shopping_cart == '1':
-                self.page_size = '999'
+                self.page_size = self.custom_limit
         return super().paginate_queryset(queryset, request, view)
